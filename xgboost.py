b8b0877969dde0422f46583e71db3ec468bec236
Nikita Chernyshov <xpom55@mail.ru>
2018-04-19 00:42:28 +0300

    useing xgboost =) and get 86% accuracy, the end of the course of Machine learning a-z =(

--- xgboost.py
+++ xgboost.py
@@ -27,6 +27,28 @@ X = X[:, 1:] # 1 индекс уберем to avoid dummy variable trap
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) # warning: solve cross_validation => model_selection
 
+# Fitting XGBoost to the Training set
+from xgboost import XGBClassifier
+classifier = XGBClassifier()
+classifier.fit(X_train, y_train)
 
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
 
+# Making the confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
 
+# Applying k-Fold Cross Validation
+# более продвинутый способ проверить точность модели
+# разбиваем тестовые данные на 10 разных вариантов
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(
+  estimator = classifier, # передаем модель для проверки
+  X = X_train,
+  y = y_train,
+  cv = 10 # количество тестовых разбивок для проверки точности модели
+  #,n_jobs = -1 # если большие данные, то можно использовать все ядра проца
+)
+accuracies.mean() # 90% accuracy from 10 numbers
+accuracies.std() # standart deviation (отклонение точностей) меньше - лучше
